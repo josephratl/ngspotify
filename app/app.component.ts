494cc09d328d23f6fe45644b7df0b1d72b0b96b1
Joseph <joseph.lama@hotmail.com>
2016-11-12 20:35:08 +0100

    refactored teh whole app

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: `<h1>Hello Angular</h1>`
+    template: `<navbar></navbar>
+        <div class="container">
+        <router-outlet></router-outlet>
+        </div>`
 })
+
 export class AppComponent { }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,29 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { routing } from './app.routing';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
-import { AppComponent }  from './app.component';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
+import { SpotifyService } from './services/spotify.service';
+import { AlbumComponent } from './components/album/album.component';
+import { ArtistComponent } from './components/artist/artist.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+    imports: [BrowserModule, routing, FormsModule, HttpModule],
+    declarations: [AppComponent,
+        NavbarComponent,
+        AboutComponent,
+        AlbumComponent,
+        ArtistComponent,
+        SearchComponent],
+    providers: [
+        SpotifyService
+    ],
+    bootstrap: [AppComponent]
 })
+
 export class AppModule { }
